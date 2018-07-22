0d69d9a3453657434871576c9a9d2b478400c403
NCMoseley <ncmoseley@gmail.com>
2018-07-21 22:23:47 -0700

    add Auth with AWS amplify

--- src/index.js
+++ src/index.js
@@ -7,6 +7,30 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'posts',
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      }
+    ]
+  }
+});
+
 ReactDOM.render(
   <Router>
     <App />
